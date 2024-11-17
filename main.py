ef7c4ad284539efa4dc587634a37a5885bb5374e
kormarg <romulus.florent.lucas@gmail.com>
2024-11-17 18:18:04 +0100

    second commit

--- main.py
+++ main.py
@@ -3,10 +3,17 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+import pandas as pd
+import numpy as np
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+df = pd.read_csv("data/survey_results_public.csv")
+print(df.columns)
+
+
 
 
 # Press the green button in the gutter to run the script.
